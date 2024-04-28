8cd88792fe506db98e6ff6a1e80995328e46e29d
Roger Howell <MysterAitch@users.noreply.github.com>
2024-04-28 09:49:29 +0100

    Tweak error handling pages

--- EquipmentBookingSystem.Website/Pages/Error.cshtml.cs
+++ EquipmentBookingSystem.Website/Pages/Error.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,6 +9,10 @@ namespace EquipmentBookingSystem.Website.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+    public int OriginalStatusCode { get; set; }
+
+    public string? OriginalPathAndQuery { get; set; }
+
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
@@ -19,9 +24,41 @@ public class ErrorModel : PageModel
         _logger = logger;
     }
 
-    public void OnGet()
+    private void Handler(int statusCode)
     {
+        OriginalStatusCode = statusCode;
+
+        var statusCodeReExecuteFeature =
+            HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+        if (statusCodeReExecuteFeature is not null)
+        {
+            OriginalPathAndQuery = string.Join(
+                statusCodeReExecuteFeature.OriginalPathBase,
+                statusCodeReExecuteFeature.OriginalPath,
+                statusCodeReExecuteFeature.OriginalQueryString);
+        }
+
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
-}
 
+    public void OnGet(int statusCode)
+    {
+        Handler(statusCode);
+    }
+
+    public void OnPost(int statusCode)
+    {
+        Handler(statusCode);
+    }
+
+    public void OnPut(int statusCode)
+    {
+        Handler(statusCode);
+    }
+
+    public void OnDelete(int statusCode)
+    {
+        Handler(statusCode);
+    }
+}
--- EquipmentBookingSystem.Website/Program.cs
+++ EquipmentBookingSystem.Website/Program.cs
@@ -36,8 +36,15 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
+    app.UseExceptionHandler("/Error");
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
