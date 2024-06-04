de4cc23da85825b29d4e3575960f914369e32ac9
Roger Howell <MysterAitch@users.noreply.github.com>
2024-06-04 15:12:09 +0100

    Display

--- EquipmentBookingSystem.Website/Models/Item.cs
+++ EquipmentBookingSystem.Website/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EquipmentBookingSystem.Website.Models;
 
@@ -13,6 +14,24 @@ public class Item : BaseEntity
     public HashSet<Booking> Bookings { get; set; } = new();
 
     public HashSet<ItemIdentifier> Identifiers { get; set; } = new();
+
+    [NotMapped]
+    public IEnumerable<ItemIdentifier> CurrentIdentifiers => Identifiers
+        .Where(x => (x.From ?? DateTime.MinValue) <= DateTime.Today)
+        .Where(x => (x.To ?? DateTime.MaxValue) > DateTime.Today)
+    ;
+
+    [NotMapped]
+    public IEnumerable<ItemIdentifier> CurrentSerialNumbers => CurrentIdentifiers.Where(i => i.Type == "Serial Number");
+
+    [NotMapped]
+    public IEnumerable<ItemIdentifier> CurrentProCloudAssetIds => CurrentIdentifiers.Where(i => i.Type == "ProCloud Asset ID");
+
+    [NotMapped]
+    public IEnumerable<ItemIdentifier> CurrentCallSigns => CurrentIdentifiers.Where(i => i.Type == "Call Sign");
+
+    [NotMapped]
+    public IEnumerable<ItemIdentifier> CurrentIssis => CurrentIdentifiers.Where(i => i.Type == "ISSI");
 
     [Display(Name = "Damage Notes")]
     [DataType(DataType.MultilineText)]
